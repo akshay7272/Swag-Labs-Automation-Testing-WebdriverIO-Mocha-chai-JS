90c2c9492d35d909d26fc3970429d2dc56f67407
akshay7272 <kashyap72akshay@gmail.com>
2025-05-16 13:39:15 +0530

    Project Setup for Swag Labs Automation Testing

---
+++ test/pageobjects/login.page.js
@@ -0,0 +1,35 @@
+/**
+ * sub page containing specific selectors and methods for a specific page
+ */
+class LoginPage {
+    /**
+     * define selectors using getter methods
+     */
+    get inputUsername () {
+        return $('#user-name');
+    }
+
+    get inputPassword () {
+        return $('#password');
+    }
+
+    get btnLogin () {
+        return $('#login-button');
+    }
+     get siteLogo () {
+        return $('.login_logo');
+    }
+
+    /**
+     * a method to encapsule automation code to interact with the page
+     * e.g. to login using username and password
+     */
+    async login (username, password) {
+        await this.inputUsername.setValue(username);
+        await this.inputPassword.setValue(password);
+        await this.btnLogin.click();
+    }
+
+}
+
+export default new LoginPage();
